45115832ea880b9c0b31618ee1c28eefbefc6f1f
matheus <matheus@matheus-Aspire-5750>
2017-01-23 13:57:22 -0200

    ViagemTable Criada para busca de viagens. Veiculos alterado para empresa.

--- src/turism/regra/ViagemTableModel.java
+++ src/turism/regra/ViagemTableModel.java
@@ -5,38 +5,39 @@
  */
 package turism.regra;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
-import turism.modelo.Veiculo;
+import turism.modelo.Viagem;
 
 /**
  *
  * @author matheus
  */
-public class VeiculosTableModel extends AbstractTableModel {
+public class ViagemTableModel extends AbstractTableModel{
+    
+    /*Formato de data*/
+    SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 
     private static final long serialVersionUID = 1L;
 
-    /* Lista de Estoque que representam as linhas. */
-    private final List<Veiculo> linhas;
+    /* Lista de Viagem que representam as linhas. */
+    private final List<Viagem> linhas;
 
     /* Array de Strings com o nome das colunas. */
     private final String[] colunas = new String[]{
-        "Empresa", "Veículo", "Tipo"};
+        "Cidade Origem: ", "Cidade Destino: ", "Data de ida: ", "Data de volta:"};
 
-    /* Cria um Veiculos Contratados vazio. */
-    public VeiculosTableModel() {
-        linhas = new ArrayList<>();
+    public ViagemTableModel() {
+        this.linhas = new ArrayList<>();
     }
 
-    /* Cria um ClienteTableModel carregado com
-     * a lista de Cliente especificada. */
-    public VeiculosTableModel(List<Veiculo> veiculos) {
-        linhas = new ArrayList<>(veiculos);
+    public ViagemTableModel(List<Viagem> viagens) {
+        linhas = new ArrayList<>(viagens);
     }
 
-    /* Retorna a quantidade de colunas. */
+        /* Retorna a quantidade de colunas. */
     @Override
     public int getColumnCount() {
         // EstÃ¡ retornando o tamanho do array "colunas".  
@@ -61,9 +62,9 @@ public class VeiculosTableModel extends AbstractTableModel {
         return String.class;
     }
     
-    @Override
+     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        Veiculo veiculo = linhas.get(rowIndex);
+        Viagem viagem = linhas.get(rowIndex);
 
         // Retorna o campo referente a coluna especificada.  
         // Aqui é feito um switch para verificar qual é a coluna  
@@ -73,11 +74,13 @@ public class VeiculosTableModel extends AbstractTableModel {
 
             // Seguindo o exemplo: "Tipo","Data de Cadastro", "Nome", "Idade"};  
             case 0:
-                return veiculo.getIdempresa().getNome();
+                return viagem.getIdorigem().getNome();
             case 1:
-                return veiculo.getNome();
+                return viagem.getIddestino().getNome();
             case 2:
-                return veiculo.getTipo();
+                return df.format(viagem.getDataida());
+            case 3:
+                return df.format(viagem.getDatavolta());
             default:
                 // Isto não deveria acontecer...  
                 throw new IndexOutOfBoundsException("columnIndex out of bounds");
@@ -87,15 +90,17 @@ public class VeiculosTableModel extends AbstractTableModel {
     @Override
 //modifica na linha e coluna especificada  
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        Veiculo veiculo = linhas.get(rowIndex); // Carrega o item da linha que deve ser modificado  
+        Viagem viagem = linhas.get(rowIndex); // Carrega o item da linha que deve ser modificado  
 
         switch (columnIndex) { // Seta o valor do campo respectivo  
             case 0:
-                veiculo.getIdempresa().getNome();
+                viagem.getIdorigem().getNome();
             case 1:
-                veiculo.getNome();
+                viagem.getIddestino().getNome();
             case 2:
-                veiculo.getTipo();
+                viagem.getDataida();
+            case 3:
+                viagem.getDatavolta();
             default:
             // Isto não deveria acontecer...               
         }
@@ -103,17 +108,18 @@ public class VeiculosTableModel extends AbstractTableModel {
     }
 
     //modifica na linha especificada  
-    public void setValueAt(Veiculo aValue, int rowIndex) {
-        Veiculo veiculo = linhas.get(rowIndex); // Carrega o item da linha que deve ser modificado  
+    public void setValueAt(Viagem aValue, int rowIndex) {
+        Viagem viagem = linhas.get(rowIndex); // Carrega o item da linha que deve ser modificado  
 
-        veiculo.getIdempresa().getNome();
-        veiculo.getNome();
-        veiculo.getTipo();
+        viagem.getIdorigem().getNome();
+        viagem.getIddestino().getNome();
+        viagem.getDataida();
+        viagem.getDatavolta();
 
         fireTableCellUpdated(rowIndex, 0);
         fireTableCellUpdated(rowIndex, 1);
         fireTableCellUpdated(rowIndex, 2);
-        //fireTableCellUpdated(rowIndex, 3);
+        fireTableCellUpdated(rowIndex, 3);
 
     }   
    
@@ -122,12 +128,12 @@ public class VeiculosTableModel extends AbstractTableModel {
         return false;
     }
 
-    public Veiculo getVeiculos(int indiceLinha) {
+    public Viagem getViagem(int indiceLinha) {
         return linhas.get(indiceLinha);
     }
 
     /* Adiciona um registro. */
-    public void addItem(Veiculo m) {
+    public void addItem(Viagem m) {
         // Adiciona o registro.  
         linhas.add(m);
 
@@ -144,12 +150,12 @@ public class VeiculosTableModel extends AbstractTableModel {
     }
 
     /* Adiciona uma lista de Cliente ao final dos registros. */
-    public void addListaVeiculo(List<Veiculo> veiculos) {
+    public void addListaViagem(List<Viagem> viagens) {
         // Pega o tamanho antigo da tabela.  
         int tamanhoAntigo = getRowCount();
 
         // Adiciona os registros.  
-        linhas.addAll(veiculos);
+        linhas.addAll(viagens);
 
         fireTableRowsInserted(tamanhoAntigo, getRowCount() - 1);
     }
@@ -165,5 +171,4 @@ public class VeiculosTableModel extends AbstractTableModel {
     public boolean isEmpty() {
         return linhas.isEmpty();
     }
-
 }
